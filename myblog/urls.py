1d55f7db8b85a8f357ee5135942d3080cf151734
zhongxq2018 <zhongxq2018@163.com>
2020-07-23 16:28:13 +0800

    add router

--- myblog/urls.py
+++ myblog/urls.py
@@ -25,6 +25,10 @@ urlpatterns = [
     path('ueditor/', include('DjangoUeditor.urls')),
     re_path('^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
-    path('', views.index),
-    path('home/', views.home),
+    path('', views.index, name='index'),
+    path('list-<int:lid>.html', views.list, name='list'),
+    path('show-<int:sid>.html', views.show, name='show'),#内容页
+    path('tag/<tag>', views.tag, name='tags'),#标签列表页
+    path('s/', views.search, name='search'),#搜索列表页
+    path('about/', views.about, name='about'),#联系我们单页
 ]
